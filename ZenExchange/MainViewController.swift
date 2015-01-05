c933028d9e51ef11a4026d5e94f5cd17e9083687
Alexander Tovstonozhenko <a.tovstonozhenko@mozidev.com>
2015-01-05 16:35:27 +0200

    Add dummy messages
    These should show up when the network is down and/or the info hasn't been fetched yet

--- ZenExchange/MainViewController.swift
+++ ZenExchange/MainViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
+let YQL_QUERY: String = "https://query.yahooapis.com/v1/public/yql?q=select%20Bid%2CAsk%2CBidRealtime%2CAskRealtime%2CName%2CSymbol%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22USDRUB%3DX%2CEURRUB%3DX%2CBZQ15.NYM%2CEURUAH%3DX%2CUSDUAH%3DX%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var tugriksPerDollarLabel: UILabel!
+    @IBOutlet weak var tugriksPerDollarCommentLabel: UILabel!
+    @IBOutlet weak var tugriksPerEuroLabel: UILabel!
+    @IBOutlet weak var tugriksPerEuroCommentLabel: UILabel!
+    @IBOutlet weak var dollarsPerBarrelLabel: UILabel!
+    @IBOutlet weak var dollarsPerBarrelCommentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        showDummyResults()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +31,18 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func showDummyResults() {
+        var dummyText: NSMutableArray = ["я чото п?", "guru meditation", "при пожаре звони 101", "не паникуй!", "в килобайте 1000 байт", "буду через 15 минут", "кто здесь?", "ж-ж-ж-ж-ж", "привет, как дела?", "my other car is a cdr"]
+        for label in [tugriksPerDollarLabel, tugriksPerEuroLabel, dollarsPerBarrelLabel] {
+            label.text = "–"
+        }
+        for label in [tugriksPerDollarCommentLabel, tugriksPerEuroCommentLabel, dollarsPerBarrelCommentLabel] {
+            let count = UInt32(dummyText.count)
+            let index = Int(arc4random_uniform(count))
+            label.text = dummyText[index] as? String ?? ""
+            dummyText.removeObjectAtIndex(index)
+        }
     }
-    */
 
 }
