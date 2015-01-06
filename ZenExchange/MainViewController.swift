660e7e5b39cf4ecad3cb0ec40a37e311add31609
Alexander Tovstonozhenko <a.tovstonozhenko@mozidev.com>
2015-01-06 12:02:50 +0200

    Refactor tugriks
    Slightly better than it was but still ugly and not flexible enough.

--- ZenExchange/MainViewController.swift
+++ ZenExchange/MainViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 let YQL_QUERY: String = "https://query.yahooapis.com/v1/public/yql?q=select%20Bid%2CAsk%2CBidRealtime%2CAskRealtime%2CName%2CSymbol%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22USDRUB%3DX%2CEURRUB%3DX%2CBZQ15.NYM%2CEURUAH%3DX%2CUSDUAH%3DX%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
 
-enum Tugrik: String {
-    case UAH = "UAH", RUB = "RUB"
+enum TugrikSymbol: String {
+    case UAH = "UAH"
+    case RUB = "RUB"
 }
+
+struct Tugrik {
+    var symbol: String
+    var name: String
+}
+
+let tugriks: [TugrikSymbol: Tugrik] = [
+    .UAH: Tugrik(symbol: TugrikSymbol.UAH.rawValue, name: "гривен"),
+    .RUB: Tugrik(symbol: TugrikSymbol.RUB.rawValue, name: "рублей")
+]
 
 class MainViewController: UIViewController {
 
@@ -23,26 +34,24 @@ class MainViewController: UIViewController {
     @IBOutlet weak var tugriksPerEuroCommentLabel: UILabel!
     @IBOutlet weak var dollarsPerBarrelCommentLabel: UILabel!
     
-    var tugrikName: String!
-    var tugriksPerDollarComment: String!
-    var tugriksPerEuroComment: String!
-    var dollarsPerBarrelComment: String!
+    var currentTugrik: TugrikSymbol! = .UAH
     
-    var currentTugrik: Tugrik = .UAH {
-        willSet {
-            switch newValue {
-            case .UAH:
-                tugrikName = "гривен"
-            case .RUB:
-                tugrikName = "рублей"
-            default:
-                break
-            }
-            tugriksPerDollarComment = tugrikName + " за доллар"
-            tugriksPerEuroComment = tugrikName + " за евро"
-            dollarsPerBarrelComment = "долларов за баррель"
+    var tugrikName: String! {
+        get {
+            return tugriks[self.currentTugrik]!.name
         }
     }
+    var tugriksPerDollarComment: String! {
+        get {
+            return tugrikName + " за доллар"
+        }
+    }
+    var tugriksPerEuroComment: String! {
+        get {
+            return tugrikName + " за евро"
+        }
+    }
+    let dollarsPerBarrelComment: String! = "долларов за баррель"
     
     
     // MARK: - Overridden methods
@@ -89,8 +98,9 @@ class MainViewController: UIViewController {
         NSURLConnection.sendAsynchronousRequest(request, queue: queue) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
             let result: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
             let parsedResults = self.parseJSON(result)
-            let dollarQuote = parsedResults.valueForKey("USD\(self.currentTugrik.rawValue)=X") as? Double ?? 0.0
-            let euroQuote = parsedResults.valueForKey("EUR\(self.currentTugrik.rawValue)=X") as? Double ?? 0.0
+            let symbol = tugriks[self.currentTugrik]!.symbol
+            let dollarQuote = parsedResults.valueForKey("USD\(symbol)=X") as? Double ?? 0.0
+            let euroQuote = parsedResults.valueForKey("EUR\(symbol)=X") as? Double ?? 0.0
             let oilQuote = parsedResults.valueForKey("BZQ15.NYM") as? Double ?? 0.0
             self.showResults(dollarQuote: dollarQuote, euroQuote: euroQuote, oilQuote: oilQuote)
         }
