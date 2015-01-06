95bd4c42acc3aea0e8bc71c275221e76a053d27d
Alexander Tovstonozhenko <a.tovstonozhenko@mozidev.com>
2015-01-06 10:37:35 +0200

    Add basic quotes fetching

--- ZenExchange/MainViewController.swift
+++ ZenExchange/MainViewController.swift
@@ -10,20 +10,51 @@ import UIKit
 
 let YQL_QUERY: String = "https://query.yahooapis.com/v1/public/yql?q=select%20Bid%2CAsk%2CBidRealtime%2CAskRealtime%2CName%2CSymbol%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22USDRUB%3DX%2CEURRUB%3DX%2CBZQ15.NYM%2CEURUAH%3DX%2CUSDUAH%3DX%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
 
+enum Tugrik: String {
+    case UAH = "UAH", RUB = "RUB"
+}
+
 class MainViewController: UIViewController {
 
     @IBOutlet weak var tugriksPerDollarLabel: UILabel!
-    @IBOutlet weak var tugriksPerDollarCommentLabel: UILabel!
     @IBOutlet weak var tugriksPerEuroLabel: UILabel!
-    @IBOutlet weak var tugriksPerEuroCommentLabel: UILabel!
     @IBOutlet weak var dollarsPerBarrelLabel: UILabel!
+    @IBOutlet weak var tugriksPerDollarCommentLabel: UILabel!
+    @IBOutlet weak var tugriksPerEuroCommentLabel: UILabel!
     @IBOutlet weak var dollarsPerBarrelCommentLabel: UILabel!
+    
+    var tugrikName: String!
+    var tugriksPerDollarComment: String!
+    var tugriksPerEuroComment: String!
+    var dollarsPerBarrelComment: String!
+    
+    var currentTugrik: Tugrik = .UAH {
+        willSet {
+            switch newValue {
+            case .UAH:
+                tugrikName = "гривен"
+            case .RUB:
+                tugrikName = "рублей"
+            default:
+                break
+            }
+            tugriksPerDollarComment = tugrikName + " за доллар"
+            tugriksPerEuroComment = tugrikName + " за евро"
+            dollarsPerBarrelComment = "долларов за баррель"
+        }
+    }
+    
+    
+    // MARK: - Overridden methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        var timer = NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: "fetchQuotes", userInfo: nil, repeats: true)
+        
         showDummyResults()
+        fetchQuotes()
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,10 +63,70 @@ class MainViewController: UIViewController {
     }
     
     
+    // MARK: - Action methods
+    
+    @IBAction func ukrainianFlagTapped(sender: AnyObject) {
+        if currentTugrik != .UAH {
+            currentTugrik = .UAH
+            fetchQuotes()
+        }
+    }
+
+    @IBAction func russianFlagTapped(sender: AnyObject) {
+        if currentTugrik != .RUB {
+            currentTugrik = .RUB
+            fetchQuotes()
+        }
+    }
+    
+    
+    // MARK: - Private methods
+    
+    func fetchQuotes() {
+        let url = NSURL(string: YQL_QUERY)!
+        let request = NSURLRequest(URL: url)
+        let queue = NSOperationQueue()
+        NSURLConnection.sendAsynchronousRequest(request, queue: queue) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            let result: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+            let parsedResults = self.parseJSON(result)
+            let dollarQuote = parsedResults.valueForKey("USD\(self.currentTugrik.rawValue)=X") as? Double ?? 0.0
+            let euroQuote = parsedResults.valueForKey("EUR\(self.currentTugrik.rawValue)=X") as? Double ?? 0.0
+            let oilQuote = parsedResults.valueForKey("BZQ15.NYM") as? Double ?? 0.0
+            self.showResults(dollarQuote: dollarQuote, euroQuote: euroQuote, oilQuote: oilQuote)
+        }
+    }
+    
+    func parseJSON(jsonObject: NSDictionary) -> NSDictionary {
+        var result = NSMutableDictionary()
+        let json = JSON(jsonObject)
+        let quotes = json["query"]["results"]["quote"]
+        for (index: String, quote: JSON) in quotes {
+            let symbol = quote["Symbol"]
+            var ask = quote["Ask"]
+            if let askRealtime = quote["AskRealtime"].string {
+                ask = quote["AskRealtime"]
+            }
+            result[symbol.string!] = (ask.string! as NSString).doubleValue
+        }
+        return result
+    }
+    
+    func showResults(#dollarQuote: Double, euroQuote: Double, oilQuote: Double) {
+        dispatch_async(dispatch_get_main_queue()) {
+            println("\(dollarQuote)\t\(euroQuote)\t\(oilQuote)")
+            self.tugriksPerDollarLabel.text = String(format: "%.2f", dollarQuote)
+            self.tugriksPerEuroLabel.text = String(format: "%.2f", euroQuote)
+            self.dollarsPerBarrelLabel.text = String(format: "%.2f", oilQuote)
+            self.tugriksPerDollarCommentLabel.text = self.tugriksPerDollarComment
+            self.tugriksPerEuroCommentLabel.text = self.tugriksPerEuroComment
+            self.dollarsPerBarrelCommentLabel.text = self.dollarsPerBarrelComment
+        }
+    }
+    
     func showDummyResults() {
-        var dummyText: NSMutableArray = ["я чото п?", "guru meditation", "при пожаре звони 101", "не паникуй!", "в килобайте 1000 байт", "буду через 15 минут", "кто здесь?", "ж-ж-ж-ж-ж", "привет, как дела?", "my other car is a cdr"]
+        var dummyText: NSMutableArray = ["я чото п?", "guru meditation", "при пожаре звони 101", "не паникуй!", "в килобайте 1000 байт", "буду через 15 минут", "кто здесь?", "ж-ж-ж-ж-ж", "привет, как дела?", "my other car is a cdr", "скорее... эх, тоска"]
         for label in [tugriksPerDollarLabel, tugriksPerEuroLabel, dollarsPerBarrelLabel] {
-            label.text = "–"
+            label.text = ""
         }
         for label in [tugriksPerDollarCommentLabel, tugriksPerEuroCommentLabel, dollarsPerBarrelCommentLabel] {
             let count = UInt32(dummyText.count)
