602b8a6975100d8b22dd80e83b5d0cc2cd3f521b
Alexander Tovstonozhenko <a.tovstonozhenko@mozidev.com>
2015-01-13 15:29:55 +0200

    Add Record data model

---
+++ ZenExchange/Record.swift
@@ -0,0 +1,21 @@
+//
+//  Record.swift
+//  ZenExchange
+//
+//  Created by Alexander Tovstonozhenko on 13.01.15.
+//  Copyright (c) 2015 Alexander Tovstonozhenko. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Record: NSManagedObject {
+
+    @NSManaged var date: NSDate
+    @NSManaged var usdUah: NSNumber
+    @NSManaged var usdRub: NSNumber
+    @NSManaged var eurUah: NSNumber
+    @NSManaged var eurRub: NSNumber
+    @NSManaged var oil: NSNumber
+
+}
